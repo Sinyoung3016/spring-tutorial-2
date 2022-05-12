d4d51c03d1cb7afe0a27c5e6cec1351f0c883d92
Fortune00 <sinyoungbok99@gmail.com>
2022-05-12 17:29:16 +0900

    [test] MemberServiceTest에 findMembers와 findOne 테스트 완료
    - 하나의 테스트 안에서는 인증된 함수만 써야겠다

--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
 
@@ -35,12 +37,12 @@ class MemberServiceTest {
         Long saveId = memberService.join(member);
 
         //then
-        Member findMember = memberService.findOne(saveId).get();
+        Member findMember = memberRepository.findById(saveId).get();
         assertThat(member.getName()).isEqualTo(findMember.getName());
     }
 
     @Test
-    void join_duplicate(){
+    void join_duplicate() {
         //given
         Member member1 = new Member();
         member1.setName("james");
@@ -55,13 +57,41 @@ class MemberServiceTest {
         IllegalStateException e = assertThrows(IllegalStateException.class,
                 () -> memberService.join(member2));
         assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다");
-
-        /*try{
-            memberService.join(member2);
-            fail();
-        }catch(IllegalStateException e){
-            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다");
-        }*/
     }
 
+    @Test
+    void findMembers() {
+        //given
+        Member member1 = new Member();
+        member1.setName("james");
+        memberRepository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("mark");
+        memberRepository.save(member2);
+
+        //when
+        List<Member> members = memberService.findMembers();
+
+        //then
+        assertThat(members.size()).isEqualTo(2);
+    }
+
+    @Test
+    void findOne() {
+        //given
+        Member member1 = new Member();
+        member1.setName("james");
+        memberRepository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("mark");
+        memberRepository.save(member2);
+
+        //when
+        Member findMember = memberService.findOne((long) 2).get();
+
+        //then
+        assertThat(findMember.getName()).isEqualTo(member2.getName());
+    }
 }
